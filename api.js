20960db5fc8be627cdda4fd1d414863b283fcf63
Ian <ian@chenghiz.net>
2021-06-18 09:45:12 -0700

    simplify error handling

--- api.js
+++ api.js
@@ -1,27 +1,19 @@
 'use strict';
-const express = require('express');
 const { getClasses, getFeats, getMagicSchools, getSpells } = require('./sqlite-client')
+const router = require('express-promise-router');
 
-const api = express()
+const api = router()
 	.get('/spells', function (req, res) {
-		getSpells()
-			.then(results => res.send(results))
-			.catch(console.error)
+		return getSpells().then(results => res.send(results))
 	})
 	.get('/classes', function (req, res) {
-		getClasses()
-			.then(results => res.send(results))
-			.catch(console.error)
+		return getClasses().then(results => res.send(results))
 	})
 	.get('/magic_schools', function (req, res) {
-		getMagicSchools()
-			.then(results => res.send(results))
-			.catch(console.error)
+		return getMagicSchools().then(results => res.send(results))
 	})
 	.get('/feats', function (req, res) {
-		getFeats()
-			.then(results => res.send(results))
-			.catch(console.error)
+		return getFeats().then(results => res.send(results))
 	});
 
 module.exports = api;
