5b61f0ef822671050e8f55aea902222710291aab
Ian <ian@chenghiz.net>
2021-06-26 14:28:23 -0700

    implement error handling

--- api.js
+++ api.js
@@ -15,7 +15,11 @@ function initialize(databaseClient) {
 		})
 		.get('/feats', function (req, res) {
 			return getFeats().then(results => res.send(results))
-		});
+		})
+		.use(function (error, req, res, next) {
+			console.error(error)
+			res.status(error.status || 500).send(error.message)
+		})
 
 	return api
 }
